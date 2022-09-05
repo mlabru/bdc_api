723c94f4246217d33ae58b822ac8f0115a105504
Milton Abrunhosa <ml_byteman@yahoo.com.br>
2022-09-05 10:01:40 -0300

    initial release

---
+++ bdc_api/api_run_db.py
@@ -0,0 +1,70 @@
+import flask
+from flask import request, jsonify
+import sqlite3
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>CLSim API</h1>
+<p>A prototype API for CLSim data.</p>'''
+
+
+@app.route('/api/v1/resources/data/all', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('data.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_data = cur.execute('SELECT * FROM data;').fetchall()
+
+    return jsonify(all_data)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/api/v1/resources/data', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM data WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('data.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+app.run()
